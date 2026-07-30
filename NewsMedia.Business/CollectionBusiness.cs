1f39a89baf5ae1bca8dddd54f39b71ce3c9ec364
yassermora <129242285+yassermora@users.noreply.github.com>
2026-07-29 22:26:02 -0600

    feat(collections): capa de negocio de Collection

---
+++ NewsMedia.Business/CollectionBusiness.cs
@@ -0,0 +1,26 @@
+﻿using NewsMedia.Models;
+using NewsMedia.Repositories;
+
+namespace NewsMedia.Business
+{
+	public class CollectionBusiness : ICollectionBusiness
+	{
+		private readonly ICollectionRepository _repo;
+
+		public CollectionBusiness(ICollectionRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public Task<List<Collection>> GetByOwnerAsync(string ownerId) => _repo.GetByOwnerAsync(ownerId);
+		public Task<Collection?> GetByIdAsync(int id, string ownerId) => _repo.GetByIdAsync(id, ownerId);
+		public Task<Collection> CreateAsync(Collection collection) => _repo.CreateAsync(collection);
+		public Task<bool> DeleteAsync(int id, string ownerId) => _repo.DeleteAsync(id, ownerId);
+
+		public Task<bool> AssignItemAsync(int collectionId, int sourceItemId, string ownerId)
+			=> _repo.AssignItemAsync(collectionId, sourceItemId, ownerId);
+
+		public Task<bool> RemoveItemAsync(int sourceItemId, string ownerId)
+			=> _repo.RemoveItemAsync(sourceItemId, ownerId);
+	}
+}
---
+++ NewsMedia.Business/ICollectionBusiness.cs
@@ -0,0 +1,14 @@
+﻿using NewsMedia.Models;
+
+namespace NewsMedia.Business
+{
+	public interface ICollectionBusiness
+	{
+		Task<List<Collection>> GetByOwnerAsync(string ownerId);
+		Task<Collection?> GetByIdAsync(int id, string ownerId);
+		Task<Collection> CreateAsync(Collection collection);
+		Task<bool> DeleteAsync(int id, string ownerId);
+		Task<bool> AssignItemAsync(int collectionId, int sourceItemId, string ownerId);
+		Task<bool> RemoveItemAsync(int sourceItemId, string ownerId);
+	}
+}
